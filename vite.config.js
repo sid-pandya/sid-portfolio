7a5fb0526564f8714eca61e19ab6424638fe0790
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2025-11-22 16:07:52 -0800

    Configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change this to '/repo-name/' if deploying to GitHub Pages project site
+  base: '/sid-portfolio/', // GitHub Pages project site
 })
